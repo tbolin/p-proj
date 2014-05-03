d47866d726b9e8e76ea794f8bd34b0bc605fce09
tbolin <tobias_bolin@hotmail.com>
2014-05-03 22:01:00 +0200

    structured
    Början på en mer uppstrukturerad version

--- main.py
+++ main.py
@@ -8,43 +8,89 @@ from random import randint
 def conv_time(start, time):
 	return "{0:02d}:{1:02d}".format(start+(time//60),time%60)
 
-def new_customer():
+def gen_errands():
 	errands = 1
 	flipp = randint(0,1)
 	while flipp:
 		errands += 1
 		flipp = randint(0,1)
-	return errands
+	return {'post':errands}
 
 class Person():
-	__id = 1
+	__id = 0
 	def __init__(self, errands=0):
-		if not errands: self.errands = new_customer()
+		if not errands: self.errands = gen_errands()
 		self.id = Person.__id
 		Person.__id += 1
+
+class Log:
+	__id = 0
+	def __init__(self, world, parrent):
+		self.id = Event.__id
+		Logg.__id s+= 1
+		self.who = []
+		self.what = []
+		self.when = []
 	
+	def add(self, person, event):
+		self.who.append(person)
+		self.what.append(event)
+		self.when.append(self.world.time())
+	
+	def __iter__(self):
+		for i, time, text in enumerate(zip(when, what)): 
+			yield conv_time(time, 9) + "\t" + text
+	
+	def __repr__(self):
+		for p in self: print(p)
+		
+		
 class facility():
-	def __init__(self):
+	def __init__(self, world, queue=[], service='post', p_time=2):
 		self.current = None
 		self.bussy = 0
-		self.eff = 2
-		self.counter = 0
+		self.log_counter = 0
+		self.queue = queue
+		self.service = service
+		self.p_time = p_time
+		self.wait_time = 0
+		self.loger = Log(world, self)
+		self.done = []
 	
-	def start_process(self, process):
-		if self.bussy: return False
-		self.current = process
-		self.bussy = self.eff * process.errands +1
-		
+	def add_client(self, client):
+		if self.service in client.errands: 
+			if self.current: self.loger(client, 'add'+len(queue)+1)
+			else: 
+			self.queue.append(client)
+	
+	def log(self, person, event):
+		self.logger.add(person,event)
+	
 	def step(self):
 		# Step forward and return True if ready for next task
-		if not self.bussy: return False
-		self.bussy -= 1
-		if not self.bussy: 
-			process = self.current
-			self.current = None
+		if self.current and not self.current.errand[self.service]:
+			self.log(self.current, 'done')
+			self.done.append(self.current)
 			self.counter += 1
-			return process
+			self.current = None
+		elif self.current:
+			self.current.errand[self.service] -= 1
+		if self.queue and self.current is None
+			self.current = self.queue.pop(0)
+			self.log(self.current, 'next')
+		self.wait_time += len(self.queue)
 		return False
+
+class World:
+	def __init__(self):
+		self.time = 0
+		self.places = []
+		self.people = []
+		
+	def step(self):
+		for f in places:
+			f.step()
+		self.tie += 1
 		
 def main():
 	open = 9
